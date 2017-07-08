8d279ace35ee6c6e6483f7faf927aedc05da436e
Yixuan Qiu <yixuanq@gmail.com>
2017-07-07 22:39:25 -0400

    loading fonts

--- R/load_fonts.R
+++ R/load_fonts.R
@@ -7,12 +7,68 @@
 #' @examples load_fonts()
 load_fonts = function()
 {
-    fontfile = system.file("fonts", "wqy-microhei.ttc.zip", package = "showtextdb")
-    outdir = tempdir()
-    unzip(fontfile, exdir = outdir)
-    outfile = file.path(outdir, "wqy-microhei.ttc")
+    load_default_font()
+    load_user_fonts()
+}
 
-    sysfonts::font.add("wqy-microhei", outfile)
+
+
+## Check if a family has been loaded
+already_loaded = function(family)
+{
+    all(family %in% sysfonts::font.families())
+}
+
+## Load the default WenQuanYi Micro Hei font
+load_default_font = function()
+{
+    ## Check if wqy-microhei has been loaded already
+    if(already_loaded("wqy-microhei"))
+        return(invisible(NULL))
+    
+    ## Extract font file
+    font_file = system.file("fonts", "wqy-microhei.ttc.zip", package = "showtextdb")
+    out_dir = tempdir()
+    out_file = file.path(out_dir, "wqy-microhei.ttc")
+    if(!file.exists(out_file))
+        utils::unzip(font_file, exdir = out_dir, overwrite = FALSE)
+    
+    ## Add font to sysfonts
+    sysfonts::font.add("wqy-microhei", out_file)
+    
+    invisible(NULL)
+}
+
+## Load fonts that have been installed by the user
+load_user_fonts = function()
+{
+    ## The directory that contains the user fonts
+    font_db = system.file("fonts", package = "showtextdb")
+    
+    ## Each folder under fonts_db is considered a user font with different font faces
+    font_dirs = list.dirs(font_db, recursive = FALSE)
+    if(!length(font_dirs))
+        return(invisible(NULL))
+    
+    ## Family names without the full path
+    family_names = basename(font_dirs)
+    ## Scan all possible font faces
+    faces = c("regular", "bold", "italic", "bolditalic", "symbol")
+    ## Add fonts one by one
+    for(i in seq_along(family_names))
+    {
+        if(!already_loaded(family_names[i]))
+        {
+            font_files = list.files(font_dirs[i])
+            args = lapply(faces, function(face) {
+                r = grep(sprintf("^%s\\.[[:alnum:]]+", face), font_files, value = TRUE)
+                if(!length(r)) NULL else file.path(font_dirs[i], r)
+            })
+            names(args) = faces
+            args = c(family = family_names[i], args)
+            do.call(sysfonts::font.add, args)
+        }
+    }
     
     invisible(NULL)
 }
